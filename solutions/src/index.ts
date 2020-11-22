268d869ec62c55c786a939092506506f0651fdb5
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-22 23:25:30 +0000

    remove solutions from master

--- solutions/src/index.ts
+++
@@ -1,4 +0,0 @@
-// istanbul ignore file
-import { add } from "@app/math";
-
-console.log(add(1, 2));
--- solutions/test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import * as App from "@app/math";
-
-describe("App Suite", () => {
-  it("add", () => {
-    const result = App.add(1, 2);
-
-    expect(result).toEqual(3);
-  });
-});
